c5d4f527a2e03475440a595790a6eb8576ad06dd
Pradumn-Singh-Patel <pradumnpatel18@gmail.com>
2024-04-14 00:16:24 +0530

    feat/added temporary db file

---
+++ routes/db.ts
@@ -0,0 +1,14 @@
+ export const data = [
+{
+    username : "Vinni",
+    password : "1234"
+},
+{
+    username : "Raj",
+    password : "123"
+},
+{
+    username : "Pp",
+    password : "12345"
+}
+]
