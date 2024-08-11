a1958162ee08d3916711048df02df12d454f3545
vinayaksaubhri <vinayaksaubhri@gmail.com>
2024-08-11 17:21:55 +0530

    feat:generateRoomId function

---
+++ utils/generateRoomId.ts
@@ -0,0 +1,4 @@
+const { customAlphabet } = require("nanoid");
+const alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const generateRoomId = customAlphabet(alphabet, 6);
+export default generateRoomId;
